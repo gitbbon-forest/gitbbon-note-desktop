4101dbd26ec7dcbc5ba976bbd3e1165e16d4098b
egoing <egoing@gmail.com>
2026-01-07 00:16:07 +0900

    fix(search): set tsconfigRootDir in eslint config

--- extensions/gitbbon-search/eslint.config.mjs
+++ extensions/gitbbon-search/eslint.config.mjs
@@ -1,18 +1,39 @@
 // @ts-check
 import tseslint from 'typescript-eslint';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default tseslint.config(
+	{
+		languageOptions: {
+			parserOptions: {
+				tsconfigRootDir: __dirname
+			}
+		}
+	},
 	...tseslint.configs.recommended,
 	{
 		files: ['src/**/*.ts'],
 		languageOptions: {
 			parser: tseslint.parser,
 			parserOptions: {
-				project: './tsconfig.json'
+				project: './tsconfig.json',
+				tsconfigRootDir: __dirname
 			}
 		},
 		rules: {
 			'header/header': 'off'
+		}
+	},
+	{
+		files: ['vite.config.ts'],
+		languageOptions: {
+			parserOptions: {
+				tsconfigRootDir: __dirname
+			}
 		}
 	}
 );
